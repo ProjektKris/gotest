53dae1a0928d93d66626beb35c4e1718ba7e7b11
ProjektKris <rbxkrisxyz@gmail.com>
2021-05-01 11:15:42 +0700

    Initialize repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ProjektKris/hello
+
+go 1.16
---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ProjektKris/hello/morestrings"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Scanln()
+}
